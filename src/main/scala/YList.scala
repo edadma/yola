dd3e5e10b359dce268e8ea8b167138916aaf207b
Edward A Maxedon Sr <edadma@gmail.com>
2020-04-10 22:54:45 -0400

    iterable types

--- src/main/scala/YList.scala
+++ src/main/scala/YList.scala
@@ -1,10 +1,16 @@
 package xyz.hyperreal.yola
 
 object YListType extends YType {
+
   val name   = "List"
   val parent = YIterableType
+
 }
 
 case class YList(v: List[Value]) extends WrappedValue[List[Value]](YListType, null) {
+
   override def toString: String = v map quoted mkString ("[", ", ", "]")
+
+  override def iterator: Iterator[Value] = v.iterator
+
 }
